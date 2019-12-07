7dabfbec85db86ed4a3b879913ace0df7d26cce3
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2019-12-07 13:54:02 -0800

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,13 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync');
-
+    browserSync = require('browser-sync');​
 var plumberErrorHandler = {
     errorHandler: notify.onError({
         title: 'Gulp',
         message: 'Error: <%= error.message %>'
     })
-};
-
+};​
 gulp.task('sass', function() {
     gulp.src('./sass/*.scss')
         .pipe(plumber(plumberErrorHandler))
@@ -20,26 +18,21 @@ gulp.task('sass', function() {
             browsers: ['last 2 versions']
         }))
         .pipe(gulp.dest('./build/css'))
-});
-
+});​
 gulp.task('scripts', function() {
     gulp.src('./js/*.js')
         .pipe(gulp.dest('./build/js'))
-});
-
+});​
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
-    });
-
+    });​
     gulp.watch(['build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
-});
-
+});​
 gulp.task('watch', function() {
-    gulp.watch('sass/*.scss', ['sass']);
-    gulp.watch('js/*.js', ['scripts']);
-});
-
-gulp.task('default', ['watch', 'browser-sync']);
+    gulp.watch('sass/*.scss', gulp.series('sass'));
+    gulp.watch('js/*.js', gulp.series('scripts'));
+});​
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
