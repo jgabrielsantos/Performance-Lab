f5e76a4344be3655204d5059052d1a175e63921d
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2019-12-07 15:20:30 -0800

    minified js and css files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync');
 var plumberErrorHandler = {
@@ -12,15 +14,15 @@ var plumberErrorHandler = {
 };
 gulp.task('sass', function() {
     gulp.src('./sass/*.scss')
-        .pipe(plumber(plumberErrorHandler))
         .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
+        .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(rename({ extname: '.min.css' }))
         .pipe(gulp.dest('./build/css'))
 });
 gulp.task('scripts', function() {
-    gulp.src('./js/*.js')
+    gulp.src('./js/**/*.js')
+        .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('./build/js'))
 });
 gulp.task('browser-sync', function() {
@@ -32,7 +34,7 @@ gulp.task('browser-sync', function() {
     gulp.watch(['build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
 });
 gulp.task('watch', function() {
-    gulp.watch('sass/*.scss', gulp.series('sass'));
-    gulp.watch('js/*.js', gulp.series('scripts'));
+    gulp.watch('./sass/*.scss', gulp.series('sass'));
+    gulp.watch('./js/**/*.js', gulp.series('scripts'));
 });
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
