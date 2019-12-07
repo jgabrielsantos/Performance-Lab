2e23936892a513f1815487e881537197364475dc
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2019-12-07 14:41:15 -0800

    compress svgs, jpeg and gif files

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync');​
+    browserSync = require('browser-sync');
 var plumberErrorHandler = {
     errorHandler: notify.onError({
         title: 'Gulp',
         message: 'Error: <%= error.message %>'
     })
-};​
+};
 gulp.task('sass', function() {
     gulp.src('./sass/*.scss')
         .pipe(plumber(plumberErrorHandler))
@@ -18,21 +18,21 @@ gulp.task('sass', function() {
             browsers: ['last 2 versions']
         }))
         .pipe(gulp.dest('./build/css'))
-});​
+});
 gulp.task('scripts', function() {
     gulp.src('./js/*.js')
         .pipe(gulp.dest('./build/js'))
-});​
+});
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
-    });​
+    });
     gulp.watch(['build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
-});​
+});
 gulp.task('watch', function() {
     gulp.watch('sass/*.scss', gulp.series('sass'));
     gulp.watch('js/*.js', gulp.series('scripts'));
-});​
+});
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
